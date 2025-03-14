59c734aa192888e8b40de3685ded909808329652
Coriluz <760351@student.dusd.net>
2025-03-14 11:42:50 -0700

    I finished the game.
    All i need to do is the video and slightly tweak the tanks.

--- Unit1PlayerControlRamirez/Assets/Scripts/PlayerController.cs
+++ Unit1PlayerControlRamirez/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 20.0f;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+    private float speed = 25.0f;
     private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
@@ -23,5 +26,10 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         //Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
